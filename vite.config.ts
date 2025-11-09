2640e27ac90714b7a1132c34d705b5c207f4aed8
Gregor Artur <arturonlineweb@gmail.com>
2025-11-09 02:23:00 +0200

    v 1.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/REPO_NAME/", // <-- add your repo name here
+  base: "/Bl-kudoku/",
   plugins: [react()],
 });
